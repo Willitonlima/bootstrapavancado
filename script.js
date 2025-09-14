c51b5072497063c0908aeb19a88761d5b7983a28
Willitonlima <willitonwilli@gmail.com>
2025-09-14 15:00:57 -0300

    correção de erros de digitação e adição de ids unicos aos campos do header

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     $('.irPara').on('click', (function (e) {
-        e.preventDefaut(); //Evita o salto direto 
+        e.preventDefault(); //Evita o salto direto 
 
         const targetId = $(this).attr('href'); //Pega as identificações de destino
 
         $('html, body').animate({
             scrollTop: $(targetId).offset().top //escrola suavemente ate o elemento q eu pedi
-        }, 800);
+        }, 200);
     }))
 });
